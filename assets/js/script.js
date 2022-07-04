0d76944cf6c31af6130cf389a1a0b327b04e4994
rchaitanyaaxioned <chaitanyar@axioned.com>
2022-07-04 12:29:19 +0530

    JS logic implemented and itis working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,19 @@
-/* Author: 
+function keyCodeGenerator(event) {
+  var eventKey = event.key;
+  var eventKeyCode = event.which;
+  var eventCode = event.code;
 
-*/
+  console.log("eventKey: " + eventKey);
+  console.log("eventKeyCode: " + eventKeyCode);
+  console.log("eventCode: " + eventCode);
 
+  document.querySelector(".first-screen").style.display = "none";
+  document.querySelector(".second-screen").style.display = "flex";
+
+  document.querySelector(".event-key").value = eventKey;
+  document.querySelector(".event-key-code").value = eventKeyCode;
+  document.querySelector(".event-code").value = eventCode;
+}
 
 
 
